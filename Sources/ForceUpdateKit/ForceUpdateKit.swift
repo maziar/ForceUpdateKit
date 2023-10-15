dfea91864c24fb3475a48ae4b890b446fa62a70c
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2023-10-15 14:44:14 +0330

    Change the window

--- Sources/ForceUpdateKit/ForceUpdateKit.swift
+++ Sources/ForceUpdateKit/ForceUpdateKit.swift
@@ -3,10 +3,10 @@
 import Foundation
 import UIKit
 public class ForceUpdateKit {
-    public static func configure(scene: UIWindowScene, config: UpdateServiceConfig = UpdateServiceConfig()) {
+    public static func configure(config: UpdateServiceConfig = UpdateServiceConfig()) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             let forceUpdateView = ForceUpdateView()
-            let window = UIWindow(windowScene: scene)
+            let window = UIApplication.shared.windows.last!
             window.addSubview(forceUpdateView)
             forceUpdateView.fixInView(window)
             window.makeKeyAndVisible()
