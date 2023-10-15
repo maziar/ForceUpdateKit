49913f2ac9d7332fedc02d228b017bf3bf1a3f00
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2023-10-15 14:37:48 +0330

    Change initializer

--- Sources/ForceUpdateKit/ForceUpdateKit.swift
+++ Sources/ForceUpdateKit/ForceUpdateKit.swift
@@ -3,14 +3,12 @@
 import Foundation
 import UIKit
 public class ForceUpdateKit {
-    public static func configure(window: UIWindow,
-                                 config: UpdateServiceConfig = UpdateServiceConfig()) {
+    public static func configure(scene: UIWindowScene, config: UpdateServiceConfig = UpdateServiceConfig()) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             let forceUpdateView = ForceUpdateView()
-            if let view = window.rootViewController?.view {
-                view.addSubview(forceUpdateView)
-                forceUpdateView.fixInView(view)
-            }
+            let window = UIWindow(windowScene: scene)
+            window.addSubview(forceUpdateView)
+            forceUpdateView.fixInView(window)
         }
     }
 }
